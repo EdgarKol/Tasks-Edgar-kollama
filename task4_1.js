a9a63f49e1d7cb9933f7b146ab559da6f77bfa76
Edgar Kollamaa <edgar.kollamaa@vikk.ee>
2021-09-16 14:39:22 +0300

    4.1

---
+++ task4_1.js
@@ -0,0 +1,10 @@
+const colors = ["Red", "Green", "White", "Black"];
+for (let i = 0; i < colors.length; i++) {
+    // console.log(colors);
+}
+let text = colors.toString();
+console.log(text);
+const f = colors.join(", ");
+console.log(f);
+const e = colors.join(" + ");
+console.log(e);
