1b75339d0958d45e3c8c63460ca45d4c28677e62
Edgar Kollamaa <edgar.kollamaa@vikk.ee>
2021-09-20 18:09:21 +0300

    undefined wont work

--- task6.js
+++ task6.js
@@ -1,13 +1,12 @@
 function hei() {
-    let n = 11
+    let n = 10
     let b = 'Hi!'
     let a = 'Hello'
     for (let i = 0; i < n; i++)
-        if (n === undefined) {
-            console.log(b);
-            break;
-        } else if (n > 10) {
-        console.log(b)
+        if (n > 10) {
+            return b
+        } else if (n === undefined) {
+        return b
     } else {
         console.log(a)
     }
