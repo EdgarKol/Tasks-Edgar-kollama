508f0fc7bea860f2d7306f5291d0a6d81db431cd
Edgar Kollamaa <edgar.kollamaa@vikk.ee>
2021-09-29 23:31:31 +0300

    uuem

--- task6.js
+++ task6.js
@@ -1,22 +1,15 @@
-function hei(n) {
+function hei() {
+    let n
     let b = 'Hi!'
     let a = 'Hello'
     for (let i = 0; i < n; i++)
         if (!n) {
-            console.log(b);
+            return b
 
-            function hei() {
-                let n
-                let b = 'Hi!'
-                let a = 'Hello'
-                for (let i = 0; i < n; i++)
-                    if (n === undefined) {
-                        return b
-
-                    } else if (n > 10) {
-                    console.log(b)
-                } else {
-                    console.log(a)
-                }
-            }
-            console.log(hei())
+        } else if (n > 10) {
+        console.log(b)
+    } else {
+        console.log(a)
+    }
+}
+console.log(hei())
